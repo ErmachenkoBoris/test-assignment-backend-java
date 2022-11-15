d98d2b931630b41cf8498ffff084e5e768a085c8
Boris Ermachenko <ermachenkoboris@gmail.com>
2022-11-15 15:35:50 +0200

    [backend assignment]: add base functions for CRUD

--- src/main/java/com/example/bookstore/model/Book.java
+++ src/main/java/com/example/bookstore/model/Book.java
@@ -1,4 +1,27 @@
 package com.example.bookstore.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "books")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long uuid;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "isbn")
+    private String isbn;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "release_date")
+    private String releaseDate;
 }
---
+++ src/main/java/com/example/bookstore/service/BookService.java
@@ -0,0 +1,40 @@
+package com.example.bookstore.service;
+
+
+import com.example.bookstore.model.Book;
+import com.example.bookstore.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Book findById(Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public Book updateBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
